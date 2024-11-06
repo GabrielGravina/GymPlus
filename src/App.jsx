de82e5671d60c1f12701ec656f4b06ae64797d5f
João Bosco <jbpedrosaf33@gmail.com>
2024-11-05 23:45:03 -0300

    Commit Bosco

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ import mapsImage from './images/maps.png';
 function App() {
   return (
     <div className='flex-auto w-screen h-screen'>
+
+      {/* Seção de boas-vindas */}
+      <header className="bg-[#00DB36] text-gray-900 py-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <h1 className="text-6xl font-extrabold text-white">GymPlus</h1>
+          <ul className="flex space-x-6 text-xl font-bold">
+            <li className="hover:bg-gray-700 cursor-pointer bg-gray-600 bg-opacity-70 p-3 text-white shadow-md rounded-md">Home</li>
+            <li className="hover:bg-gray-700 cursor-pointer bg-gray-600 bg-opacity-70 p-3 text-white shadow-md rounded-md">Entrar</li>
+            <li className="hover:bg-gray-700 cursor-pointer bg-gray-600 bg-opacity-70 p-3 text-white shadow-md rounded-md">Sobre</li>
+          </ul>
+        </div>
+      </header>
+
       {/* Navbar com imagem de capa cobrindo toda a navbar */}
       <nav
         className="bg-cover bg-center text-white p-4 h-1/2"
@@ -13,70 +26,76 @@ function App() {
           backgroundSize: 'cover', // Cobrir toda a navbar
           backgroundPosition: 'center',
         }}
-      >
-        <div className="container mx-auto flex items-center justify-between">
-          <div className="text-5xl font-bold bg-slate-900 bg-opacity-30 p-2 rounded-md">GymPlus</div>
-          <ul className="flex space-x-6 text-xl font-bold">
-            <li className="text-xl hover:bg-slate-400 cursor-pointer bg-slate-900 bg-opacity-70 p-3 shadow-md rounded-md">Home</li>
-            <li className="text-xl hover:bg-slate-400 cursor-pointer bg-slate-900 bg-opacity-70 p-3 shadow-md rounded-md">Entrar</li>
-            <li className="text-xl hover:bg-slate-400 cursor-pointer bg-slate-900 bg-opacity-70 p-3 text-white shadow-md rounded-md">Sobre</li>
-          </ul>
-        </div>
-      </nav>
-
-      {/* Seção de boas-vindas */}
-      <header className="bg-gray-800 text-white py-20">
-        <h1 className="text-center text-6xl font-extrabold mb-6">Bem-vindo à Nossa Academia</h1>
-        <p className="text-center text-xl font-light">Treine com os melhores equipamentos e acompanhamento profissional.</p>
-      </header>
+      />
 
       {/* Conteúdo principal */}
       <main className="py-10">
         <section className="container mx-auto text-center">
-          <h2 className="text-4xl font-bold mb-4">Nossos Serviços</h2>
-          <p className="text-lg font-light mb-8">Oferecemos treinos personalizados, musculação, aulas de cardio e muito mais.</p>
+          <h2 className="text-4xl font-bold mb-4 text-[#00DB36]">Nossos Serviços</h2>
+          <p className="text-xl font-light mb-8 text-white">Oferecemos treinos personalizados, localização de academias próximas, horários de funcionamento das atividades, feedbacks e muito mais.</p>
+
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div className="p-6 bg-gray-200 text-slate-900 rounded shadow-lg">
-              <h3 className="text-2xl font-semibold mb-2">Musculação</h3>
-              <p className="font-light">Fortaleça seu corpo com os melhores equipamentos.</p>
-              <button className="cursor-pointer border border-[#3498db] bg-transparent h-12 w-48 text-[#3498db] text-xl shadow-lg hover:bg-[#3498db] hover:text-white transition-all">
-                Musculação
+            <div className="p-6 bg-[#00DB36] text-gray-900 rounded shadow-lg">
+              <h3 className="text-2xl font-semibold mb-2">Academias</h3>
+              <p className="font-semibold text-black">Veja informações sobre as academias próximas a você, buscando facilitar a sua decisão.</p>
+              <button className="cursor-pointer font-semibold border border-[#2D8643] bg-transparent h-12 w-full max-w-[250px] text-black text-xl shadow-lg hover:bg-[#2D8643] hover:text-white transition-all mt-2">
+                Encontrar Academias
               </button>
             </div>
-            <div className="p-6 bg-gray-200 text-slate-900 rounded shadow-lg">
+            <div className="p-6 bg-[#00DB36] text-gray-900 rounded shadow-lg">
+              <h3 className="text-2xl font-semibold mb-2">Monte o seu treino</h3>
+              <p className="font-semibold text-black">Fortaleça seu corpo com os melhores equipamentos nas academias que mais façam sentido para você.</p>
+              <button className="cursor-pointer font-semibold border border-[#2D8643] bg-transparent h-12 w-full max-w-[250px] text-black text-xl shadow-lg hover:bg-[#2D8643] hover:text-white transition-all mt-2">
+                Treino
+              </button>
+            </div>
+            <div className="p-6 bg-[#00DB36] text-gray-900 rounded shadow-lg">
               <h3 className="text-2xl font-semibold mb-2">Personal Trainer</h3>
-              <p className="font-light">Acompanhamento individual para atingir seus objetivos.</p>
-              <button className="cursor-pointer border border-[#3498db] bg-transparent h-12 w-48 text-[#3498db] text-xl shadow-lg hover:bg-[#3498db] hover:text-white transition-all">
+              <p className="font-semibold text-black">Encontre um profissional com um acompanhamento individual que faça sentido para atingir seus objetivos.</p>
+              <button className="cursor-pointer font-semibold border border-[#2D8643] bg-transparent h-12 w-full max-w-[250px] text-black text-xl shadow-lg hover:bg-[#2D8643] hover:text-white transition-all mt-2">
                 Personal Trainer
               </button>
             </div>
-            <div className="p-6 bg-gray-200 text-slate-900 rounded shadow-lg">
-              <h3 className="text-2xl font-semibold mb-2">Aulas de Cardio</h3>
-              <p className="font-light">Queime calorias e melhore sua resistência com nossas aulas.</p>
-              <button className="cursor-pointer border border-[#3498db] bg-transparent h-12 w-48 text-[#3498db] text-xl shadow-lg hover:bg-[#3498db] hover:text-white transition-all">
-                Aulas de Cardio
+            <div className="p-6 bg-[#00DB36] text-gray-900 rounded shadow-lg">
+              <h3 className="text-2xl font-semibold mb-2">Feedback</h3>
+              <p className=" font-semibold  text-black">Acompanhe feedbacks de alunos da academia que você deseja frequentar.</p>
+              <button className="cursor-pointer font-semibold border border-[#2D8643] bg-transparent h-12 w-full max-w-[250px] text-black text-xl shadow-lg hover:bg-[#2D8643] hover:text-white transition-all mt-2">
+                Feedback
+              </button>
+            </div>
+            <div className="p-6 bg-[#00DB36] text-gray-900 rounded shadow-lg">
+              <h3 className="text-2xl font-semibold mb-2">Ver atividades</h3>
+              <p className=" font-semibold  text-black">Veja as atividades que a academia que você deseja entrar disponibiliza.</p>
+              <button className="cursor-pointer font-semibold border border-[#2D8643] bg-transparent h-12 w-full max-w-[250px] text-black text-xl shadow-lg hover:bg-[#2D8643] hover:text-white transition-all mt-2">
+                Atividades
+              </button>
+            </div>
+            <div className="p-6 bg-[#00DB36] text-gray-900 rounded shadow-lg">
+              <h3 className="text-2xl font-semibold mb-2">Ver horários</h3>
+              <p className=" font-semibold  text-black">Veja o horário das atividades da academia que você deseja entrar.</p>
+              <button className="cursor-pointer font-semibold border border-[#2D8643] bg-transparent h-12 w-full max-w-[250px] text-xl shadow-lg hover:bg-[#2D8643] hover:text-white transition-all mt-2 text-black">
+                Horários
               </button>
             </div>
           </div>
         </section>
 
-        
-        {/* Nova seção: Ver Planos de Academias */}
+        {/* Nova seção: Cadastro */}
         <section className="container mx-auto text-center mt-10">
-          <h2 className="text-4xl font-bold mb-4">Ver Planos de Academias</h2>
+          <h2 className="text-4xl font-bold mb-10 text-[#00DB36]">Faça parte do GymPlus!</h2>
           <div className="flex justify-center space-x-6">
-            <button className="cursor-pointer border border-[#3498db] bg-transparent h-12 w-48 text-[#3498db] text-xl shadow-lg hover:bg-[#3498db] hover:text-white transition-all">
-              Ver Planos de Musculação
+            <button className="cursor-pointer border border-[#2D8643] bg-transparent h-auto w-52 p-4  text-white text-lg shadow-lg hover:bg-[#2D8643] font-semibold hover:text-white transition-all">
+              Entrar como aluno
             </button>
-            <button className="cursor-pointer border border-[#3498db] bg-transparent h-12 w-48 text-[#3498db] text-xl shadow-lg hover:bg-[#3498db] hover:text-white transition-all">
-              Ver Planos de Yoga
+            <button className="cursor-pointer border border-[#2D8643] bg-transparent h-auto w-52 p-4 text-white text-lg shadow-lg hover:bg-[#2D8643] font-semibold hover:text-white transition-all">
+              Entrar como professor
             </button>
-            <button className="cursor-pointer border border-[#3498db] bg-transparent h-12 w-48 text-[#3498db] text-xl shadow-lg hover:bg-[#3498db] hover:text-white transition-all">
-              Ver Planos de Cardio
+            <button className="cursor-pointer border border-[#2D8643] bg-transparent h-auto w-52 p-4 text-white text-lg shadow-lg hover:bg-[#2D8643] font-semibold hover:text-white transition-all">
+              Entrar como academia
             </button>
           </div>
         </section>
-        
+
         {/* Imagem de fundo do homem malhando */}
         <div className="text-center mt-10">
           <img src={gymManImage} alt="Homem Malhando" className="mx-auto h-96 object-cover" />
@@ -85,14 +104,13 @@ function App() {
 
       {/* Nova seção: Encontre Academias */}
       <section className="container mx-auto text-center mt-2 mb-20">
-          <h2 className="text-4xl font-bold mb-4">Encontre Academias</h2>
-          <img src={mapsImage} alt="Mapa" className="mx-auto h-96 object-cover mt-10" />
-        </section>
-
+        <h2 className="text-4xl font-bold mb-4 text-[#00DB36]">Encontre academias próximas à você!</h2>
+        <img src={mapsImage} alt="Mapa" className="mx-auto h-96 object-cover mt-10" />
+      </section>
 
       {/* Rodapé */}
-      <footer className="bg-gray-900 text-white py-4 text-center">
-        <p>&copy; 2024 Nossa Academia. Todos os direitos reservados.</p>
+      <footer className="bg-[#2D8643] text-white py-4 text-center">
+        <p>&copy; 2024 GymPlus. Todos os direitos reservados.</p>
       </footer>
     </div>
   );
